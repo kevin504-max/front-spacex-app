9e89ee87c6472f8a213e3e09c24bd14b68e6d28c
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-08-02 18:49:44 -0300

    feat: initial styles

--- src/components/SpaceXComponent.vue
+++ src/components/SpaceXComponent.vue
@@ -1,6 +1,22 @@
 <template>
-    <div>
-        <h1>Space X component</h1>
+    <div class="main">
+        <div class="header">
+            <h1><i class="fas fa-rocket icon"></i> Space X</h1>
+        </div>
+        <div class="charts">
+            <div class="chart">
+                <h2>Lançamentos de foguetes</h2>
+                
+            </div>
+            <div class="chart">
+                <h2>Lançamentos por ano</h2>
+
+            </div>
+        </div>
+        <div class="header">
+            <h2>Registros de lançamentos</h2>
+            
+        </div>
     </div>
 </template>
 
@@ -10,6 +26,4 @@
     }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" src="./style.scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = 'http://127.0.0.1:3300/api/'
